f908f2c7cd526fb43d6a3c96998b4013d9538c79
Oumatou <oumatoulacen@gmail.com>
2024-08-24 01:24:16 +0100

    mvp

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from os import getenv
+load_dotenv()  # Automatically loads .env file
+
+# Config example
+class Config:
+    SECRET_KEY = getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = getenv('SQLALCHEMY_DATABASE_URI')
+    DEBUG = getenv('DEBUG')
+    # ...
